fa59cf23e53b7ae7af5f29e37753011e81396d6f
TITANxTCA <80444511+TITANxTCA@users.noreply.github.com>
2022-04-05 19:41:51 +0530

    changes go brrrrrrr

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random()*6+1);
 var randomNumber2 = Math.floor(Math.random()*6+1);
 
-document.getElementsByTagName('img')[0].setAttribute('src', `/images/dice${randomNumber1}.png`);
-document.getElementsByTagName('img')[1].setAttribute('src', `/images/dice${randomNumber2}.png`);
+document.getElementsByTagName('img')[0].setAttribute('src', `images/dice${randomNumber1}.png`);
+document.getElementsByTagName('img')[1].setAttribute('src', `images/dice${randomNumber2}.png`);
 
 if (randomNumber1 > randomNumber2){
     document.querySelector('h1').innerHTML = "Player 1 wins!";
